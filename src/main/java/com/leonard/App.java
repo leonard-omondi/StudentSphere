354575722fac7064e5b656297b5a8fd9f160204c
omondileonard <lomondi@students.kennesaw.edu>
2024-01-01 11:26:14 -0500

    Initial project setup

---
+++ src/main/java/com/leonard/App.java
@@ -0,0 +1,13 @@
+package com.leonard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
