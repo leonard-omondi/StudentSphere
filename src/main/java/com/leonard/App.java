f3720824bb18a398e31ac23c10d886473af35e9d
omondileonard <lomondi@students.kennesaw.edu>
2024-01-01 11:48:29 -0500

    Dependencies updated.

--- src/main/java/com/leonard/App.java
+++ src/main/java/com/leonard/App.java
@@ -1,13 +1,11 @@
 package com.leonard;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Javalin app = Javalin.create().start(9000);
+
     }
 }
